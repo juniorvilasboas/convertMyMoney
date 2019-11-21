2f006746a84bfe559e76c73a4c9127e410adc029
Moacyr Santana <junior.vilasboas@gmail.com>
2019-11-20 23:47:13 -0300

    Versão inicial com tratativa de erros

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # convertMyMoney
 Sistema de conversão de valores
+
+Sistema desenvolvido em parceria com a <a href="http://www.devpleno.com">DevPleno</a>, no curso Fullstack Master
---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+const convert = require('./lib/convert')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req,res) => {
+    res.render('home')
+})
+
+app.get('/cotacao', (req,res) => {
+    const { cotacao, quantidade } = req.query
+
+    if (cotacao && quantidade) {
+        const conversao = convert.convert(cotacao, quantidade)
+        
+        res.render('cotacao', {
+            error: false,
+            cotacao: convert.toMoney(cotacao),
+            quantidade: convert.toMoney(quantidade),
+            conversao: convert.toMoney(conversao)
+        })
+    } else {
+        res.render('cotacao', {
+            error: 'Valores inválidos!'
+        })
+    }
+})
+
+app.listen(port, err => {
+    if(err) {
+        console.log('Não foi possível iniciar servidor!')
+    }
+})
